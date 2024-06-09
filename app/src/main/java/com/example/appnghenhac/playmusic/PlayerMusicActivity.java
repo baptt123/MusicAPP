45d30c06bb49bbf1334ebace8d9911cea3266cb5
SV-Thuc2003 <21130557@st.hcmuaf.edu.vn>
2024-06-09 22:19:22 +0700

    update View player_music and downloaded_music

---
+++ app/src/main/java/com/example/appnghenhac/playmusic/PlayerMusicActivity.java
@@ -0,0 +1,16 @@
+package com.example.appnghenhac.playmusic;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.appnghenhac.R;
+
+public class PlayerMusicActivity extends AppCompatActivity {
+//    private ActivityMainBinding mBinding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.player_music);
+//        initView();
+
+    }
+}

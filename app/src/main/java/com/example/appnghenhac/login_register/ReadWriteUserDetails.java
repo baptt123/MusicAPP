bc38ebe6beb46ae0471c6dbb039b3f4610c46498
TaiVo21 <21130524@st.hcmuaf.edu.vn>
2024-06-17 20:57:56 +0700

    login,register,logout, forgot password, update profile, upload picture

---
+++ app/src/main/java/com/example/appnghenhac/login_register/ReadWriteUserDetails.java
@@ -0,0 +1,14 @@
+package com.example.appnghenhac.login_register;
+
+public class ReadWriteUserDetails {
+    public String birthDate, gender, phoneNumber;
+    public ReadWriteUserDetails(){
+
+    }
+    public ReadWriteUserDetails( String textGender, String textBirthDate, String textPhoneNum) {
+        this.gender = textGender;
+        this.birthDate = textBirthDate;
+        this.phoneNumber = textPhoneNum;
+
+    }
+}
